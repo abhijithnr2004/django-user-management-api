b463e72c4e7714ca5b5f48277016d1cbdbbb3fff
ashik <ashikvincent6@gmail.com>
2026-01-28 22:40:09 +0530

    Users model created

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Users(models.Model):
+
+    first_name = models.CharField(max_length=255)
+
+    last_name = models.CharField(max_length=255)
+
+    user_name = models.CharField(max_length=255, unique=True)
+
+    email = models.EmailField(unique=True)
+
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+    bio = models.TextField(blank=True)
+
+    password = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
